717b90cd6a3a18fbd5860fbbf05034a705ac66d6
Ani Dal <zbaker985@gmail.com>
2023-03-29 13:23:46 -0500

    Created GUI base

--- Green_Thumb_GUI.py
+++ Green_Thumb_GUI.py
@@ -1,4 +1,11 @@
-from Tkinter import *
+#
+
+from tkinter import *
+
+#constants
+WIDTH = 800
+HEIGHT = 500
+FONTSIZE = 40
 
 class MainGUI(Frame):
     def __init__(self, parent):
@@ -7,6 +14,18 @@ class MainGUI(Frame):
     
     #configure GUI
     def setupGUI(self):
+        self.display = Label(self, text = "", anchor = E, bg = "white", height = HEIGHT, width = WIDTH, font = ("", FONTSIZE))
+        self.display.grid(row = 0, column = 0, columnspan = 4, sticky = E+W+N+S)
+    
+    #detect and respond to input from user on GUI
+    def processInput(self, button):
         pass
-    
-    
+
+######################  MAIN  ########################################
+#create window
+window = Tk()
+window.title("Green Thumb")
+#generate GUI
+p = MainGUI(window)
+#display and wait for user interaction
+window.mainloop()
