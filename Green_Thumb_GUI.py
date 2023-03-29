56c0b0199ae1d45847f063d84c96e7de08c970f7
Ani Dal <zbaker985@gmail.com>
2023-03-29 12:15:44 -0500

    uploaded GUI screens rough draft

---
+++ Green_Thumb_GUI.py
@@ -0,0 +1,12 @@
+from Tkinter import *
+
+class MainGUI(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent, bg = "white")
+        self.setupGUI()
+    
+    #configure GUI
+    def setupGUI(self):
+        pass
+    
+    
