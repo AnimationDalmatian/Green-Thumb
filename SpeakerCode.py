99f0e32b8ba7888672ded8ff09de5905ca93831b
Kelsie <83608520+FoxxKidd@users.noreply.github.com>
2023-04-27 17:13:37 -0500

    Update SpeakerCode.py

--- SpeakerCode.py
+++ SpeakerCode.py
@@ -1,12 +1,19 @@
-import RPi.GPIO as GPIO
-from time import sleep
-from random import randint
-import pygame
+import pyttsx3
+engine = pyttsx3.init() # object creation
 
-#using pygame mixer to play audio file
-soundfiles = ["one.wav", "two.wav", "three.wav", "four.wav"]
-sounds = [pygame.mixer.Sound(name) for name in soundfiles]
+def LowWater():
+    """VOLUME"""
+    volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)                        
+    engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
 
+    """VOICE"""
+    voices = engine.getProperty('voices')       #getting details of current voice
+    #engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+    engine.setProperty('voice', voices[0].id)   #changing index, changes voices. 1 for female
+
+    engine.say("Please water me, ive gone without water for too long.")
+    engine.runAndWait()
+    engine.stop()
 
 
 
